96f1182a7982a4abe3ed6af174c95ba837029641
Pablo Lara <larabjj@gmail.com>
2018-10-17 03:49:01 +0200

    First PR - S02

---
+++ pablo_lara/session-02/exercise-03/commands.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Create namespace
+kubectl create -f ns.yaml
+## Create secrets for mariadb,wordpress
+kubectl create -f mariadb-secret.yaml
+kubectl create -f wordpress-secret.yaml
+## Create ConfigMap
+kubectl create -f mariadb-config.yaml
+kubectl create -f cm.yaml
+## Create services for mariadb, wordpress
+kubectl create -f mariadb-svc.yaml
+kubectl create -f wordpress-svc.yaml
+## Create mariadb-statefulset and pvc
+kubectl create -f mariadb-statefulset.yaml
+kubectl create -f mariadb-pvc.yaml
+## Create wordpress-deployment and pvc
+kubectl create -f wordpress-pvc.yaml
+kubectl create -f wp-pvc-bk.yaml
+#kubectl create -f wordpress-pvc-data.yaml
+kubectl create -f wordpress-deployment.yaml
+
+
+kubectl create -f wordpress-ingress.yaml
+
+kubectl get svc -n exercise-03
